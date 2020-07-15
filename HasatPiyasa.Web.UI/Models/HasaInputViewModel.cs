afe6b0a598d31499707c8114f9560ea44870bdde
levent topçu <levent19782003@gmail.com>
2020-07-15 20:24:31 +0300

    update

--- HasatPiyasa.Web.UI/Models/HasaInputViewModel.cs
+++ HasatPiyasa.Web.UI/Models/HasaInputViewModel.cs
@@ -7,7 +7,7 @@ namespace HasatPiyasa_Web_UI.Models
     public class HasaInputViewModel
     {
         public Emteas Emteas { get; set; }
-        public EmteaGroups EmteaGroups { get; set; };
+        public EmteaGroups EmteaGroups { get; set; }
         public EmteaTypeGroups EmteaTypeGroups { get; set; }
         public EmteaTypes EmteaTypes { get; set; }
 
