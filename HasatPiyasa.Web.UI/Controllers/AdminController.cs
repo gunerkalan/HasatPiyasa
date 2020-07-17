53a4701bc7d42760cca6219045b9771a618b0b9e
guner kalan <gunerkalan@gmail.com>
2020-07-17 19:16:32 +0300

    emtea list and add

--- HasatPiyasa.Web.UI/Controllers/AdminController.cs
+++ HasatPiyasa.Web.UI/Controllers/AdminController.cs
@@ -2,17 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DevExpress.DirectX.Common.Direct2D;
+using HasatPiyasa.Business.Abstract;
+using HasatPiyasa.Entity.Entity;
+using HasatPiyasa.Web.UI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HasatPiyasa.Web.UI.Controllers
 {
-    public class AdminController : Controller
+    public class AdminController : BaseController
     {
+        private IEmteaService _emteaService;
+
+        public AdminController(IEmteaService emteaService)
+        {
+            _emteaService = emteaService;
+        }
+
       # region Emtea işlemleri
 
+        [HttpGet]
         public ActionResult EmteaList()
         {
-            return View();
+            EmteaModel model = new EmteaModel
+            {
+                Emtea = new Emteas()
+            };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public object EmteaListData()
+        {
+            var res = _emteaService.ListAllEmteas().Veri;
+
+            return JsonConvert.SerializeObject(res);
         }
 
 
