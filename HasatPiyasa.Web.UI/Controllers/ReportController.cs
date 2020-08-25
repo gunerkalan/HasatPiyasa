d8acfb7b3a139cf9bec9b5dd20ef04018123a434
hakanyilmaz08@windowslive.com <hakanyilmaz08@windowslive.com>
2020-08-25 15:00:37 +0300

    Bolge View Eklendi

--- HasatPiyasa.Web.UI/Controllers/ReportController.cs
+++ HasatPiyasa.Web.UI/Controllers/ReportController.cs
@@ -110,6 +110,85 @@ namespace HasatPiyasa.Web.UI.Controllers
         }
 
         [HttpGet]
+        public async Task<ActionResult> RiceGeneralReportByBolge()
+        {
+            var model = new HasaInputViewModel();
+            var _cities = await _subeCityService.GetSbCityGTable();
+            model.CitiesRapor = _cities.Veri.Distinct().ToList();
+            var _dates = await _formDataInputService.GetTable();
+            model.DateInputs = _dates.Select(x => x.AddedTime.Date).Distinct().ToList();
+            var emtea = await _emteaService.GetEmteaTable(1, 0);
+            model.Emteas = emtea.Veri;
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> RiceGeneralReportByBolgePartial(string[] dates, string[] cities, bool allDate, bool allcities)
+        {
+            var model = new HasaInputViewModel();
+            var emtea = await _emteaService.GetEmteaTable(1, 1);
+            var dataInputs = _dataInputService.ListAllDataInputs().Veri;
+            model.Emteas = emtea.Veri;
+            foreach (var item in model.Emteas.EmteaGroups)
+            {
+                foreach (var emteaTypes in item.EmteaTypes)
+                {
+                    if (!allcities && !allDate)
+                    {
+                        if (dates.Count() > 0)
+                        {
+                            emteaTypes.DataInputs = dataInputs.Where(x => dates.Contains(x.AddedTime.ToShortDateString()) && x.EmteaTypeId == emteaTypes.Id).ToList();
+                        }
+
+                        if (cities.Count() > 0)
+                        {
+                            emteaTypes.DataInputs = dataInputs.Where(x => cities.Contains(x.SubeId.ToString()) && x.EmteaTypeId == emteaTypes.Id).ToList();
+                        }
+
+                        if (cities.Count() > 0 && dates.Count() > 0)
+                        {
+                            emteaTypes.DataInputs = dataInputs.Where(x => cities.Contains(x.SubeId.ToString()) && dates.Contains(x.AddedTime.ToShortDateString()) && x.EmteaTypeId == emteaTypes.Id).ToList();
+                        }
+                    }
+                    else
+                    {
+                        if (allcities)
+                        {
+                            if (dates.Count() > 0)
+                            {
+                                emteaTypes.DataInputs = dataInputs.Where(x => dates.Contains(x.AddedTime.ToShortDateString()) && x.EmteaTypeId == emteaTypes.Id).ToList();
+                            }
+                            else
+                            {
+                                emteaTypes.DataInputs = dataInputs.Where(x => x.EmteaTypeId == emteaTypes.Id).ToList();
+                            }
+                        }
+
+                        if (allDate)
+                        {
+                            if (cities.Count() > 0)
+                            {
+                                emteaTypes.DataInputs = dataInputs.Where(x => cities.Contains(x.SubeId.ToString()) && x.EmteaTypeId == emteaTypes.Id).ToList();
+                            }
+                            else
+                            {
+                                emteaTypes.DataInputs = dataInputs.Where(x => x.EmteaTypeId == emteaTypes.Id).ToList();
+                            }
+                        }
+                    }
+
+                    if (allcities && allDate)
+                    {
+                        emteaTypes.DataInputs = dataInputs.Where(x => x.EmteaTypeId == emteaTypes.Id).ToList();
+                    }
+
+                }
+
+            }
+            return PartialView(model);
+        }
+
+        [HttpGet]
         public async Task<ActionResult> RiceGeneralReportByCity()
         {
             var model = new HasaInputViewModel();
